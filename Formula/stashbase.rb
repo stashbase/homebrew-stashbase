b6616f1093e7827f20b1fc68741ff32541409dfa
Radim Höfer <radim.engineering@stashbase.dev>
2026-07-12 13:09:47 +0200

    Update Stashbase CLI to version 0.6.1 with updated URLs and SHA256 checksums

--- Formula/stashbase.rb
+++ Formula/stashbase.rb
@@ -1,19 +1,19 @@
 class Stashbase < Formula
   desc "The official Stashbase CLI"
   homepage "https://stashbase.dev"
-  version "0.6.0"
+  version "0.6.1"
 
   on_macos do
     on_arm do
-      url "https://github.com/stashbase/cli/releases/download/v0.6.0/stashbase-0.6.0-aarch64-apple-darwin.tar.gz"
-      sha256 "0253a5784b051ec11b49b9721b02b3207f5174392d3cdadae7d468e8fff9f514"
+      url "https://github.com/stashbase/cli/releases/download/v0.6.1/stashbase-0.6.1-aarch64-apple-darwin.tar.gz"
+      sha256 "063e98bed16dbba534dec062910b5ca08b6a403e35dc98bfcad6038cf213d7f2"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/stashbase/cli/releases/download/v0.6.0/stashbase-0.6.0-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "1a610eccda10f4731413489158574baefc03125325186a9ca9286b0d392f9652"
+      url "https://github.com/stashbase/cli/releases/download/v0.6.1/stashbase-0.6.1-x86_64-unknown-linux-musl.tar.gz"
+      sha256 "ccfcabfec80336431988b12c3bb6b157c4b9999403cb4f5994172af68e41a28b"
     end
   end
 
